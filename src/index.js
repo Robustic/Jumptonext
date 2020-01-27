1e2b543397bf422cb1c3ffc3c79b194e7079504a
Juha Malinen <juha.malinen@helsinki.fi>
2020-01-27 09:57:13 +0200

    Apollo client package added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient, { gql } from 'apollo-boost'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+})
+
+const query = gql`
+{
+    stops {
+      gtfsId
+      name
+      lat
+      lon
+      zoneId
+    }
+  }
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+const App = () => {
+  return <div>
+    test
+  </div>
+}
+
+// const App = () => (
+//   <div class="container">
+//     <p>Hello world</p>
+//   </div>
+// )
+
+ReactDOM.render(<App />, document.getElementById('root'))
