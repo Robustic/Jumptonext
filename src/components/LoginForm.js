7e7c6fe4e92cf39eb5bd54dcfdbb410232fc501a
Robustic <juhas.malinen@gmail.com>
2023-08-31 18:43:25 +0300

    Login form updated to use react-bootstrap.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Button, Form, InputGroup } from 'react-bootstrap'
 
 import { LOGIN, GET_ME } from '../queries/queries'
 
@@ -46,25 +47,35 @@ const LoginForm = ({ clientDb, user, setUser, form, setForm }) => {
     }
 
     return (
-        <div>
-            <form onSubmit={submit}>
-                <div>
-                    username{' '}
-                    <input
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Form onSubmit={submit} style={{ width: 200 }}>
+                <Form.Group>
+                    <InputGroup
+                        style={{ paddingTop: 10, paddingBottom: 10 }}
                         value={username}
                         onChange={({ target }) => setUsername(target.value)}
-                    />
-                </div>
-                <div>
-                    password{' '}
-                    <input
-                        type="password"
+                    >
+                        <Form.Control type="text" placeholder="Username" />
+                    </InputGroup>
+                    <InputGroup
+                        style={{ paddingBottom: 10 }}
                         value={password}
                         onChange={({ target }) => setPassword(target.value)}
-                    />
-                </div>
-                <button type="submit">login</button>
-            </form>
+                    >
+                        <Form.Control type="password" placeholder="Password" />
+                    </InputGroup>
+                    <Button
+                        type="submit"
+                        style={{
+                            width: '100%',
+                            marginTop: 5,
+                            marginBottom: 10,
+                        }}
+                    >
+                        Login
+                    </Button>
+                </Form.Group>
+            </Form>
         </div>
     )
 }
